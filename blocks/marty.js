dbe1aba8666aa56d7c6ce492690df3daac84c2d9
Angus Pearson <a7cp3ar50n@gmail.com>
2018-04-12 14:00:20 +0100

    Add simple walk and kick blocks

--- blocks/marty.js
+++ blocks/marty.js
@@ -20,67 +20,109 @@ goog.require('Blockly');
  * Should be the same as Blockly.Msg.MARTY_HUE.
  * @readonly
  */
-Blockly.Constants.Marty.HUE = 198;
+Blockly.Constants.Marty.HUE = 40;
 /** @deprecated Use Blockly.Constants.Marty.HUE */
 Blockly.Blocks.marty.HUE = Blockly.Constants.Marty.HUE;
 
 Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
-  // Block for hello.
-  {
-    "type": "marty_hello",
-    "message0": "Hello",
-    "previousStatement": null,
+    // Block for hello.
+    {
+        "type": "marty_hello",
+        "message0": "Hello",
+        "previousStatement": null,
   	"nextStatement": null,
-    /*
-    "args0": [
-      {
-        "type": "field_variable",
-        "name": "VAR",
-        "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
-      }
-    ],
-    */
-    //"output": null,
-    //"colour": "%{BKY_VARIABLES_HUE}",
-    "colour": "198",
-    "helpUrl": "%{BKY_VARIABLES_GET_HELPURL}",
-    "tooltip": "%{BKY_VARIABLES_GET_TOOLTIP}",
-    //"extensions": ["contextMenu_variableSetterGetter"]
-  },
-  {
-  	"type": "marty_walk_forward",
-  	"message0": "Walk forward %1 steps. step length: %2, turn: %3, step time: %4",
+        /*
+          "args0": [
+          {
+          "type": "field_variable",
+          "name": "VAR",
+          "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
+          }
+          ],
+        */
+        //"output": null,
+        //"colour": "%{BKY_VARIABLES_HUE}",
+        "colour": "%{BKY_MARTY_HUE}",
+        "helpUrl": "%{BKY_VARIABLES_GET_HELPURL}",
+        "tooltip": "%{BKY_VARIABLES_GET_TOOLTIP}",
+        //"extensions": ["contextMenu_variableSetterGetter"]
+    },{
+  	"type": "marty_simple_walk",
+  	"message0": "Walk forward %1 steps",
   	"args0": [{
-      "type": "field_number",
-      "name": "STEPS",
-      "value": 2,
-      "min": 0,
-      "precision": 1
-    },{
-      "type": "field_number",
-      "name": "STEP_LENGTH",
-      "value": 40,
-      "min": 0,
-      "max": 100,
-      "precision": 1
-    },{
-      "type": "field_number",
-      "name": "TURN",
-      "value": 0,
-      "min": -100,
-      "max": 100,
-      "precision": 1
-    },{
-      "type": "field_number",
-      "name": "STEP_TIME",
-      "value": 1.8,
-      "min": 0,
-      "precision": 0.1
-    }],
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": "%{BKY_MARTY_HUE}"
+            "type": "field_number",
+            "name": "STEPS",
+            "value": 2,
+            "min": 0,
+            "precision": 1
+        }],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_MARTY_HUE}"
 
-  }
+    },{
+  	"type": "marty_walk",
+  	"message0": "Walk %1 steps of length %2, turning %3, step time: %4",
+  	"args0": [{
+            "type": "field_number",
+            "name": "STEPS",
+            "value": 2,
+            "min": 0,
+            "precision": 1
+        },{
+            "type": "field_number",
+            "name": "STEP_LENGTH",
+            "value": 40,
+            "min": 0,
+            "max": 100,
+            "precision": 1
+        },{
+            "type": "field_number",
+            "name": "TURN",
+            "value": 0,
+            "min": -100,
+            "max": 100,
+            "precision": 1
+        },{
+            "type": "field_number",
+            "name": "STEP_TIME",
+            "value": 1.8,
+            "min": 0,
+            "precision": 0.1
+        }],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_MARTY_HUE}"
+
+    },{
+  	"type": "marty_kick",
+  	"message0": "Kick on %1 side with %2 twist in step time %3",
+  	"args0": [
+            {
+                "type": "field_dropdown",
+                "name": "SIDE",
+                "value": "LEFT",
+                "options": [
+                    [ "left item", "left" ],
+                    [ "right item", "right" ]
+                ]
+            },{
+                "type": "field_number",
+                "name": "TWIST",
+                "value": 0,
+                "min": -100,
+                "max": 100,
+                "precision": 1
+            },{
+                "type": "field_number",
+                "name": "MOVE_TIME",
+                "value": 1.8,
+                "min": 0,
+                "precision": 0.1
+            }],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "%{BKY_MARTY_HUE}"
+    }
 
 ]);  // END JSON EXTRACT (Do not delete this comment.)
--- generators/javascript/marty.js
+++ generators/javascript/marty.js
@@ -10,22 +10,47 @@ goog.require('Blockly.JavaScript');
 
 
 Blockly.JavaScript['marty_hello'] = function(block) {
-  // Say hello to Marty.
-  //var code = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('VAR'),
-  //    Blockly.Variables.NAME_TYPE);
-  //return [code, Blockly.JavaScript.ORDER_ATOMIC];
-  return 'marty.hello(1);';
+    // Say hello to Marty.
+    //var code = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('VAR'),
+    //    Blockly.Variables.NAME_TYPE);
+    //return [code, Blockly.JavaScript.ORDER_ATOMIC];
+    return 'marty.hello(1);';
 };
 
-Blockly.JavaScript['marty_walk_forward'] = function(block){
-  if (block.getField('STEPS')) {
-    // Internal number.
-    var steps = String(Number(block.getFieldValue('STEPS')));
-    var step_length = String(Number(block.getFieldValue('STEP_LENGTH')));
-    var step_time = String(Number(block.getFieldValue('STEP_TIME')));
-    var turn = String(Number(block.getFieldValue('TURN')));
-  } else {
+Blockly.JavaScript['marty_walk'] = function(block){
+    if (block.getField('STEPS')) {
+        // Internal number.
+        var steps = String(Number(block.getFieldValue('STEPS')));
+        var step_length = String(Number(block.getFieldValue('STEP_LENGTH')));
+        var step_time = String(Number(block.getFieldValue('STEP_TIME')));
+        var turn = String(Number(block.getFieldValue('TURN')));
+    } else {
   	return 'console.log(\'Number of steps not found\');';
-  }
-  return 'console.log(\'marty.walk(' + steps + ')\');marty.walk(' + steps + ', ' + turn + ', ' + step_time*1000 +', ' + step_length + ');';
+    }
+    return 'marty.walk(' + steps + ', ' + turn + ', ' + step_time*1000 +', ' + step_length + ');';
+};
+
+Blockly.JavaScript['marty_simple_walk'] = function(block){
+    if (block.getField('STEPS')) {
+        // Internal number.
+        var steps = String(Number(block.getFieldValue('STEPS')));
+    } else {
+  	return 'console.log(\'Number of steps not found\');';
+    }
+    
+    var step_length = '40';
+    var step_time = '1.8';
+    var turn = '0';
+    return 'marty.walk(' + steps + ', ' + turn + ', ' + step_time*1000 +', ' + step_length + ');';
+};
+
+Blockly.JavaScript['marty_kick'] = function(block){
+    if (block.getField('SIDE')) {
+        var side = String(block.getFieldValue('SIDE'));
+        var twist = String(Number(block.getFieldValue('TWIST')));
+        var move_time = String(Number(block.getFieldValue('MOVE_TIME')));
+    } else {
+  	return 'console.log(\'Kick not found\');';
+    }
+    return 'marty.kick(\'' + side + '\', ' + twist + ', ' + move_time*1000 + ');';
 }
